f2cf2a5918cf54b8690a8870bfb04b273f8b89b5
Justin <justeen2198@gmail.com>
2020-10-25 00:58:56 -0400

    First commit

---
+++ Lab4/Models/Community.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class Community
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Required]
+        [Display(Name = "Registration Number")]
+        public string ID { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 50, MinimumLength = 5)]
+        public string Title { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        public decimal Budget { get; set; }
+
+        public ICollection<CommunityMembership> CommunityMemberships { get; set; }
+    }
+}
